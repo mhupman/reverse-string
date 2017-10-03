873cc9cf588727fc804f1c26c4a342c612d027e6
Matt Hupman <mhupman@yext.com>
2017-10-03 18:40:04 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reverse-string package
+
+Reverses selected text.  If the selected text spans multiple lines, then each line will be reversed individually.
---
+++ spec/reverse-string-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ReverseStringView from '../lib/reverse-string-view';
+
+describe('ReverseStringView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
